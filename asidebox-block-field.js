a9fbd65d1bf03bf7d4c08f379b5d684b6699821a
Derrek Lemar Croney <derrek.croney@duke.edu>
2013-05-20 10:13:44 -0400

    support for saving block module and delta

--- asidebox-block-field.js
+++ asidebox-block-field.js
@@ -5,11 +5,21 @@
 	Drupal.behaviors.asidebox_block_field = {
 		attach: function (context, settings) {
 			if (Drupal.jsAC) {
-				Drupal.jsAC.prototype.select = function(block) {
-					debugger;
+				Drupal.jsAC.prototype.select = function(node) {
 					var $blockModuleField = $('#asideboxBlockModule'),
 							$blockDeltaField = $('#asideboxBlockDelta');
 					
+					pieces = $(node).data('autocompleteValue').split('|');
+					$blockModuleField.attr('defaultValue', pieces[1]);
+					$blockDeltaField.attr('defaultValue', pieces[2]);
+					$('#asideboxBlockID').attr('defaultValue', pieces[0]);
+
+					$(this.input).val( $(node).text() );
+
+					// in order to prevent the jsAC.hidePopup function from erasing the value
+					// just placed in the textfield, set 'selected' to FALSE
+					// this = jsAC instance
+					this.selected = false;
 				}
 			}
 		}
